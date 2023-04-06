f34d37c6077b24fb2dc3c7a79d06709d3e87eec4
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-05 20:30:47 -0600

    using type assertion to input HTML Element

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,10 @@
 //le dice a typescript que no se preocupe por si es null o no
 //lo pongo al final de una expresion que podria ser null
 var btn = document.getElementById("btn");
+var input = document.getElementById("todoinput");
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    alert("CLICKED!");
+    alert(input.value);
+    input.value = "";
 });
-var mystery = "hello world";
-var numChars = mystery.length;
+// let mystery:unknown = "hello world";
+// const numChars = (mystery as string).length
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@
 //le dice a typescript que no se preocupe por si es null o no
 //lo pongo al final de una expresion que podria ser null
 const btn = document.getElementById("btn")!;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+
+
 
 btn?.addEventListener("click", function() {
-    alert("CLICKED!")
+    alert(input.value);
+    input.value = "";
 });
 
-let mystery:unknown = "hello world";
+// let mystery:unknown = "hello world";
 
 
-const numChars = (mystery as string).length
+// const numChars = (mystery as string).length
 
