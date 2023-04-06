f459a4259e76f8dac901c05a3e4f53724ae1da1f
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-05 20:16:23 -0600

    non-null assertion operator

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 //using the non-null assertion operator
 //le dice a typescript que no se preocupe por si es null o no
+//lo pongo al final de una expresion que podria ser null
 var btn = document.getElementById("btn");
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
     alert("CLICKED!");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //using the non-null assertion operator
 //le dice a typescript que no se preocupe por si es null o no
+//lo pongo al final de una expresion que podria ser null
 const btn = document.getElementById("btn")!;
 
 btn?.addEventListener("click", function() {
