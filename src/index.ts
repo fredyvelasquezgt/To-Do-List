ad82bac515217b065b8fb724e485f824c1f3f566
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-05 20:35:19 -0600

    created form wrapped button and input

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 //using the non-null assertion operator
 //le dice a typescript que no se preocupe por si es null o no
 //lo pongo al final de una expresion que podria ser null
-const btn = document.getElementById("btn")!;
+const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 
 
